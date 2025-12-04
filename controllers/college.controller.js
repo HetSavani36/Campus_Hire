b1e3e483e66f5e1e6b37023dbddc7ceff91d440d
HetSavani36 <hetsavani36@gmail.com>
2025-12-04 14:46:42 +0530

    implemented mentor detail controller

--- controllers/college.controller.js
+++ controllers/college.controller.js
@@ -371,12 +371,58 @@ const mentorDetails=asyncHandler(async(req,res)=>{
     const {mentorId}=req.params
     if(!mentorId) throw new ApiError(403,"please provide mentor id")
     
+    const college=await prisma.college.findUnique({
+      where:{email:req.user.email}
+    })
+    if(!college) throw new ApiError(404,"no such college found")
+    
     const mentor=await prisma.mentor.findUnique({
-      where:{id:mentorId}
+      where:{id:mentorId},
+      select:{
+        id:true,
+        collegeId:true,
+        user:{
+            select:{
+              id:true,
+              name:true,
+              email:true,
+            }
+        }
+      }
     })  
     if(!mentor) throw new ApiError(404,"no such employee found")
-    
-    
+
+    if(mentor.collegeId!==college.id) throw new ApiError(403,"you cant access mentor details of another college")
+
+    const allocatedJob=await prisma.job.findMany({
+        where:{
+          dueDate:{gte:new Date()},
+          collegeId:college.id,
+          isApproved:true,
+          mentorId:mentor.id
+        },
+        select:{
+          id:true,
+          title:true,
+          salary:true,
+          tenure:true,
+          address:true,
+          dueDate:true,
+          company:{
+            select:{
+              id:true,
+              name:true,
+              address:true,
+              email:true,
+              contactNo:true
+            }
+          }
+        }
+    })
+
+    res.json(
+      new ApiResponse(200,{mentor:mentor,allocatedJob:allocatedJob},"mentor detail")
+    )
 })
 
 
