74c199c4478b530826bd04fe0a42ecb0946048c6
HetSavani36 <hetsavani36@gmail.com>
2026-03-28 09:57:44 +0530

    redis conenction change

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,13 @@
-import IORedis from "ioredis";
+import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const redisConnection = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  maxRetriesPerRequest: null, // REQUIRED for BullMQ
+export const redisConnection = new Redis(process.env.REDIS_URL,{
+  tls:{
+    rejectUnauthorized:false
+  },
+  family:4
 });
+
+redisConnection.on("connect", () => console.log("Connected to Upstash Redis!"));
+redisConnection.on("error", (err) => console.log("Redis error:", err));
