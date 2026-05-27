5e3871a899ae686459485b8a83c8ffb1cf78e272
HetSavani36 <hetsavani36@gmail.com>
2026-05-27 12:29:02 +0530

    updated redis config

--- config/redis.js
+++ config/redis.js
@@ -1,13 +1,35 @@
 import Redis from "ioredis";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-export const redisConnection = new Redis(process.env.REDIS_URL,{
-  tls:{
-    rejectUnauthorized:false
-  },
-  family:4
+export const redisConnection = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
 });
 
-redisConnection.on("connect", () => console.log("Connected to Upstash Redis!"));
-redisConnection.on("error", (err) => console.log("Redis error:", err));
+redisConnection.on("connect", () => {
+  console.log("Connected to Docker Redis!");
+});
+
+redisConnection.on("error", (err) => {
+  console.log("Redis error:", err);
+});
+
+
+
+// upstash
+
+// import Redis from "ioredis";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const redisConnection = new Redis(process.env.REDIS_URL,{
+//   tls:{
+//     rejectUnauthorized:false
+//   },
+//   family:4
+// });
+
+// redisConnection.on("connect", () => console.log("Connected to Upstash Redis!"));
+// redisConnection.on("error", (err) => console.log("Redis error:", err));
